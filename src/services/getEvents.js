d519ae77cce657eb6c1378a5b71fb48caeb6f6dd
germansayago <german.sayago@gmail.com>
2022-08-24 14:44:01 -0300

    added api and styles

--- src/services/getEvents.js
+++ src/services/getEvents.js
@@ -4,9 +4,12 @@ const fromApiResponseToEvents = (apiResponse) => {
     const events = data.map((event) => {
       return {
         //TODO: format date
-        // publication_date: event.publication_date,
+        publication_date: event.publication_date,
         title: event.title,
-        date: event.start_date,
+        date: event.start_date + " " + event.start_time,
+        start: event.start_date + " " + event.start_time,
+        end: event.end_date + " " + event.end_time,
+        url: `https://destinoriocuarto.gob.ar/evento/${event.id}/${event.slug}?utm_source=calendar&utm_medium=referral`,
       };
     });
     return events;
