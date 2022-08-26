63ab57d464c6b92ddebea7bdaf52ca1ed14e7968
German Sayago <german.sayago@gmail.com>
2022-08-26 12:09:20 -0300

    fix imports

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
